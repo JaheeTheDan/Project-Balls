e816c152d783efa836da37b214faccef55e48974
JaheeTheDan <109768744+JaheeTheDan@users.noreply.github.com>
2022-09-11 16:42:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-import os
-import sys
 import keyboard
 from just_playback import Playback
 
-application_path = os.path.dirname(sys.executable)
-print(application_path)
-# application_path = os.path.dirname(sys.prefix)
-# print(application_path)
-
-
-# audio_file\balls2.wav
-file = application_path + '\\audio_file\\balls.wav'
+file = 'balls.wav' # This the audio file (you can choose any audio at your will as long as it works the just_playback library and the name is the same as audio file name)
 playback = Playback()
 playback.load_file(file)
 
-keyboard.add_word_listener('balls', playback.play, ['space', 'enter'])
+keyword = 'balls # the keyword can anything you choose
+keyboard.add_word_listener(keyword, playback.play, ['space', 'enter']) # The space and enter is used to trigger a check if the keyword is match with what is type
 print('program ready')
 while True:
-    # recorded = keyboard.record(until='enter')
-    # words = list(keyboard.get_typed_strings(recorded))
-    # for i in words:
-    #     if 'balls' in i: 
-    #         playback.play()
-    keyboard.wait()
+    keyboard.wait() # This ensures that the code doesn't stop running
